d3626ec18c308288461bd5cbb59e127cbf214024
Arturio186 <bain.romanoff@mail.ru>
2024-06-15 02:13:39 +0500

    operator -> owner; owner -> operator

--- operator/src/main.tsx
+++ operator/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import AppRouter from "#routes/AppRouter";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
--- operator/vite.config.ts
+++ operator/vite.config.ts
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '#assets': '/src/assets',
-      '#components': '/src/components',
-      '#interfaces': '/src/interfaces',
-      '#layouts': '/src/layouts',
-      '#pages': '/src/pages',
-      '#routes': '/src/routes',
-      '#store': '/src/store',
-      '#utils': '/src/utils',
-      '#hooks': '/src/hooks',
-    }
-  }
 })
