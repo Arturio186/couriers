d8ad078ebf798bd77791033a3b51d7ce7f3a00bc
Arturio186 <bain.romanoff@mail.ru>
2024-06-22 14:29:34 +0500

    logger

--- server/src/Middlewares/ErrorMiddleware.ts
+++ server/src/Middlewares/ErrorMiddleware.ts
@@ -1,11 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 
+import Logger from "../Utilities/Logger";
+
 import APIError from "../Exceptions/APIError";
 
 const ErrorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof APIError) {
+        Logger.error("API Error: ", {
+            status: error.status,
+            message: error.message,
+            stack: error.stack,
+            errors: error.errors,
+        });
         return res.status(error.status).json({message: error.message, errors: error.errors})
     }
+
+    Logger.error("Internal Error: ", {
+        status: 500,
+        message: error.message,
+        stack: error.stack,
+    });
 
     return res.status(500).json({message: "Непредвиденная ошибка"});
 }
