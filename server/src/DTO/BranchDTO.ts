acf5ffce9d6747c62fbb81ec4d863cb32276d903
Arturio186 <bain.romanoff@mail.ru>
2024-07-01 02:10:32 +0500

    City name server

--- server/src/DTO/BranchDTO.ts
+++ server/src/DTO/BranchDTO.ts
@@ -4,11 +4,13 @@ import ICoords from "../Interfaces/ICoord";
 class BranchDTO {
     public id: string;
     public name: string;
+    public city_name: string;
     public coords: ICoords
     
     constructor(branch: IBranch) {
         this.id = branch.id
         this.name = branch.name
+        this.city_name = branch.city_name
         this.coords = branch.city_coords
     }   
 }
--- server/src/Models/BranchModel.ts
+++ server/src/Models/BranchModel.ts
@@ -5,6 +5,7 @@ import IBranchModel from "../Interfaces/Branch/IBranchModel";
 
 class BranchModel implements IBranchModel {
     private readonly tableName = "branches";
+    private readonly cityTableName = "cities"
 
     public Create = async (branch: IBranch): Promise<IBranch> => {
         const [newBranch] = await db(this.tableName).insert(branch).returning<IBranch[]>("*");
@@ -29,12 +30,24 @@ class BranchModel implements IBranchModel {
     }
 
     public FindAll = async (conditions: Partial<IBranch>) : Promise<IBranch[] | undefined> => {
-        return db(this.tableName).where(conditions).orderBy("created_at", "desc");
+        return db(this.tableName)
+            .join(this.cityTableName, `${this.cityTableName}.id`, '=', `${this.tableName}.city_id`)
+            .where(conditions)
+            .select(`${this.tableName}.*`, `${this.cityTableName}.name as city_name`)
+            .orderBy("created_at", "desc");
     }
 
     public FindOne = async (conditions: Partial<IBranch>): Promise<IBranch | undefined> => {
         return db(this.tableName).where(conditions).first();
     }
+
+    /*
+     return db(this.tableName)
+                .join('roles', 'roles.id', '=', 'users.role_id')
+                .where(qualifiedConditions)
+                .select('users.*', 'roles.name as role')
+                .first();
+    */
 }
 
 export default new BranchModel();
