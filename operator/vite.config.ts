dc65feda1658a1b3840bfcd1e770c03946632050
Arturio186 <bain.romanoff@mail.ru>
2024-07-06 21:34:25 +0500

    Config

--- operator/vite.config.ts
+++ operator/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '#assets': '/src/assets',
+      '#components': '/src/components',
+      '#hooks': '/src/hooks',
+      '#http': '/src/http',
+      '#interfaces': '/src/interfaces',
+      '#layouts': '/src/layouts',
+      '#pages': '/src/pages',
+      '#routes': '/src/routes',
+      '#services': '/src/services',
+      '#store': '/src/store',
+      '#utils': '/src/utils',
+    }
+  }
 })
